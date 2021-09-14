e7c2bd2cb4c9b26e1521647f81dba5cfd6a69ce0
LuidyMonteiro <la.buenomonteiro@gmail.com>
2021-09-13 23:30:45 -0300

    Atualização MDText

--- main.py
+++ main.py
@@ -43,12 +43,14 @@ Screen:
             pos_hint: {'center_x': .5, 'center_y': .6}
             size_hint_x: .9
             hint_text: 'Nova senha:'
+            helper_text_mode: "on_focus"
+            password: True
         MDTextField:
             pos_hint: {'center_x': .5, 'center_y': .4}
             size_hint_x: .9
             hint_text: 'Confirmar nova senha:'
-            helper_text: "This will disappear when you click off"
             helper_text_mode: "on_focus"
+            password: True
         ButtonFocus:
             pos_hint: {'center_x': .5, 'center_y': .2}
             size_hint_x: .9
@@ -61,29 +63,43 @@ Screen:
         pos_hint: {'center_x': .5, 'center_y': .8}
         icon: 'account'
         user_font_size: '75sp'
-    MDTextField:
-        size_hint_x: .9
+    MDTextFieldRound:
+        icon_left: 'email'
+        size_hint_x: .8
         hint_text: 'E-mail:'
-        helper_text: "username@servidor.com"
-        helper_text_mode: "on_focus"
         pos_hint: {'center_x': .5, 'center_y': .6}
-    MDTextField:
-        size_hint_x: .9
+    MDTextFieldRound:
+        id: text_field
+        icon_left: 'key-variant'
+        size_hint_x: .8
         hint_text: 'Senha:'
-        helper_text_mode: "on_focus"
         pos_hint: {'center_x': .5, 'center_y': .5}
+        password: True
+    MDIconButton:
+        icon: "eye-off"
+        ripple_scale: .5
+        pos_hint: {'center_x': .85, 'center_y': .5}
+        on_release:
+            self.icon = "eye" if self.icon == "eye-off" else "eye-off"
+            text_field.password = False if text_field.password is True else True
     ButtonFocus:
         size_hint_x: .9
         pos_hint: {'center_x': .5, 'center_y': .4}
         text: 'Login'
         focus_color: app.theme_cls.accent_color
         unfocus_color: app.theme_cls.primary_color
+    ButtonFocus:
+        size_hint_x: .9
+        pos_hint: {'center_x': .5, 'center_y': .29}
+        text: 'Cadastrar'
+        focus_color: app.theme_cls.accent_color
+        unfocus_color: app.theme_cls.primary_color
     MDLabel:
-        pos_hint: {'center_y': .3}
+        pos_hint: {'center_y': .15}
         halign: 'center'
         text: 'Esqueceu sua senha?'
     MDTextButton:
-        pos_hint: {'center_x': .5, 'center_y': .2}
+        pos_hint: {'center_x': .5, 'center_y': .1}
         text: 'Clique Aqui'
         on_release: root.abrir_card()
 '''
