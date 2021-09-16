3061d72b6d97c30602d60bf8855a3937277de043
skymetal359 <70665450+skymetal359@users.noreply.github.com>
2021-09-16 18:43:39 -0300

    Add files via upload

---
+++ Cockpit.py
@@ -0,0 +1,31 @@
+from kivymd.app import MDApp
+from kivy.app import App
+from kivymd.uix.card import MDCard
+from kivymd.uix.floatlayout import FloatLayout
+from kivymd.uix.button import MDRaisedButton
+from kivy.lang import Builder
+from kivymd.uix.behaviors import FocusBehavior
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
+class ButtonFocus(MDRaisedButton, FocusBehavior):
+  ...
+
+class TelaManager(ScreenManager):
+    pass
+
+class MenuCockpit_Frutas(Screen):
+    pass
+class MenuCockpit_principal(Screen):
+    pass
+class MenuCockpit_Bebidas(Screen):
+    pass
+class MenuCockpit_Massas(Screen):
+    pass
+class MenuCockpit_Derivados(Screen):
+    pass
+
+class Main(MDApp):
+    def build(self):
+        return TelaManager()
+Main().run()
+
